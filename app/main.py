09671783622a4d3426d7ecc6ecacd1de1d25b4d4
Manal <manalzer69@gmail.com>
2025-12-22 19:59:03 +0100

    Add root endpoint and customize FastAPI documentation

--- app/main.py
+++ app/main.py
@@ -7,7 +7,8 @@ app = FastAPI(
     title="Incident Manager API",
     version="1.0.0"
 )
-
+# endpoint racine 
+@app.get("/")
 @app.get("/", response_class=HTMLResponse)
 def root():
     return """
